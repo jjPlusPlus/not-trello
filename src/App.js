ccc506f1f3b414b7b01b6d1ecb601a4b9fe8181a
JJ <justinjcode@gmail.com>
2019-10-19 15:11:51 -0700

    Change the title of the app... still a working title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="rapid-react-task-manager">
       <div className="top-bar">
-        <h1>Rapid React Task Manager</h1>
+        <h1>Not Trello</h1>
       </div>
       <Board />
     </div>
