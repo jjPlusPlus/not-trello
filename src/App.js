4339b1028f2ff5b40897de7caaa8db78dad62c37
JJ <justinjcode@gmail.com>
2019-10-20 20:21:43 -0700

    Start working on a generalized modal component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import './App.scss';
+import { connect } from 'react-redux';
 
 import Board from './components/Board';
-function App() {
+import ModalWrapper from './components/ModalWrapper';
+import CardDetail from './components/CardDetail';
+
+function App(props) {
+
   return (
     <div className="rapid-react-task-manager">
       <div className="top-bar">
         <h1>Not Trello</h1>
+        {props.modal.modal === true ? (
+          <ModalWrapper>
+            <CardDetail />
+          </ModalWrapper>
+        ) : (
+          null
+        )}
+
       </div>
       <Board />
+
     </div>
   );
 }
+// passing the entire state
+const mapStateToProps = state => ({
+  modal: state.modal
+})
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
