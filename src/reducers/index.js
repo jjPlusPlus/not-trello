5c55be766ad6adad46686e02bd27e94081b1eed1
JJ <justinjcode@gmail.com>
2019-10-20 19:37:40 -0700

    add a new modal action/reducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import columns from './columns';
 import cards from './cards';
+import modal from './modal';
 
-export default combineReducers({columns, cards});
+export default combineReducers({columns, cards, modal});
---
+++ src/reducers/modal.js
@@ -0,0 +1,21 @@
+import uuid from 'uuid';
+const initialState = null;
+export default function(state = { modal: null }, action) {
+  switch (action.type) {
+    case "OPEN_MODAL": {
+      return {
+        ...state,
+        modal: true
+      }
+    }
+    case "CLOSE_MODAL": {
+      return {
+        ...state,
+        modal: false
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+}
