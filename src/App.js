095030206fe8e191ce9b902ff98874c677a8ef94
JJ <justinjcode@gmail.com>
2019-10-23 09:03:24 -0700

    Move the new column button to the top bar

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import { connect } from 'react-redux';
 import Board from './components/Board';
 import ModalWrapper from './components/ModalWrapper';
 import CardDetail from './components/CardDetail';
+import { newColumn } from './actions';
 
 function App(props) {
 
   return (
-    <div className="rapid-react-task-manager">
-      <div className="top-bar">
-        <h1>Not Trello</h1>
-        {props.modal.modal === true ? (
-          <ModalWrapper>
-            <CardDetail />
-          </ModalWrapper>
-        ) : (
-          null
-        )}
-
+    <div className="not-trello">
+      <div className="top-bar p-4 fixed w-full bg-purple-800 text-yellow-400 flex flex-row shadow-2xl">
+        <h1 className="text-4xl flex-grow">Not Trello</h1>
+        <button onClick={props.newColumn}>ADD COLUMN</button>
       </div>
+      {props.modal.modal === true ? (
+        <ModalWrapper>
+          <CardDetail />
+        </ModalWrapper>
+      ) : (
+        null
+      )}
       <Board />
 
     </div>
@@ -32,7 +33,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-
+  newColumn: () => dispatch(newColumn()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
