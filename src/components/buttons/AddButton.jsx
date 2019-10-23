f0dcbe145a663d9fd889e36b4bb5e68755598529
JJ <justinjcode@gmail.com>
2019-10-23 09:04:19 -0700

    improve the move & add buttons by passing in classes

--- src/components/buttons/AddButton.jsx
+++ src/components/buttons/AddButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function AddButton(props) {
-  return <button className="add-button" onClick={() => props.action()}>Add +</button>
+  return <button className={"add-button " + props.extraClasses} onClick={() => props.action()}>Add +</button>
 }
 
 AddButton.propTypes = {
