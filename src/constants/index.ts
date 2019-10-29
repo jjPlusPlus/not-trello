fb21995f97d6bbc35ccb6ca7b3a088668ef3fc33
JJ <justinjcode@gmail.com>
2019-10-29 00:11:26 -0700

    Finally broke down and made a "constants" file

---
+++ src/constants/index.ts
@@ -0,0 +1,50 @@
+export const NEW_COLUMN = "NEW_COLUMN";
+export type NEW_COLUMN = typeof NEW_COLUMN;
+
+export const ADD_COLUMN = "ADD_COLUMN";
+export type ADD_COLUMN = typeof ADD_COLUMN;
+
+export const REMOVE_COLUMN = "REMOVE_COLUMN";
+export type REMOVE_COLUMN = typeof REMOVE_COLUMN;
+
+export const MOVE_COLUMN = "MOVE_COLUMN";
+export type MOVE_COLUMN = typeof MOVE_COLUMN;
+
+export const UPDATE_COLUMN = "UPDATE_COLUMN";
+export type UPDATE_COLUMN = typeof UPDATE_COLUMN;
+
+export const NEW_CARD = "NEW_CARD";
+export type NEW_CARD = typeof NEW_CARD;
+
+export const ADD_CARD = "ADD_CARD";
+export type ADD_CARD = typeof ADD_CARD;
+
+export const REMOVE_CARD = "REMOVE_CARD";
+export type REMOVE_CARD = typeof REMOVE_CARD;
+
+export const MOVE_CARD_HORIZONTAL = "MOVE_CARD_HORIZONTAL";
+export type MOVE_CARD_HORIZONTAL = typeof MOVE_CARD_HORIZONTAL;
+
+export const MOVE_CARD_VERTICAL = "MOVE_CARD_VERTICAL";
+export type MOVE_CARD_VERTICAL = typeof MOVE_CARD_VERTICAL;
+
+export const DRAG_DROP_CARD = "DRAG_DROP_CARD";
+export type DRAG_DROP_CARD = typeof DRAG_DROP_CARD;
+
+export const UPDATE_CARD = "UPDATE_CARD";
+export type UPDATE_CARD = typeof UPDATE_CARD;
+
+export const OPEN_CARD_DETAIL = "OPEN_CARD_DETAIL";
+export type OPEN_CARD_DETAIL = typeof OPEN_CARD_DETAIL;
+
+export const OPEN_CARD = "OPEN_CARD";
+export type OPEN_CARD = typeof OPEN_CARD;
+
+export const OPEN_MODAL = "OPEN_MODAL";
+export type OPEN_MODAL = typeof OPEN_MODAL;
+
+export const CLOSE_MODAL = "CLOSE_MODAL";
+export type CLOSE_MODAL = typeof CLOSE_MODAL;
+
+export const LOG_CARD_ACTIVITY = "LOG_CARD_ACTIVITY";
+export type LOG_CARD_ACTIVITY = typeof LOG_CARD_ACTIVITY;
