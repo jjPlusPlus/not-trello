409821e9485cd970fcec266157f7fe533ceb2d86
JJ <justinjcode@gmail.com>
2019-11-04 14:15:03 -0800

    version control didn't catch the case mismatch of the file name change

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
 import configureStore from './store';
-import './app.scss';
+import './App.scss';
 import './styles/tailwind.css';
 
 
