1f0e936f0ba2e1cd37a4fd34291373a3374a5f8a
JJ <justinjcode@gmail.com>
2019-12-14 23:01:11 -0800

    update App component new routing, remove ts, add topbar and home components

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+import { connect } from "react-redux";
+// import { AnyAction, Dispatch } from "redux";
+
+import Board from "./components/Board";
+import ModalWrapper from "./components/ModalWrapper";
+import Home from "./components/pages/Home";
+import Test from "./components/Test";
+
+import CardDetail from "./components/CardDetail";
+import TopBar from "./components/TopBar";
+
+// import { AppState } from "./types";
+
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+
+// interface DispatchProps {
+//   newColumn: () => void;
+//   modal: boolean;
+//   firebase: any;
+// }
+
+const App = (props) => {
+
+// const App: React.FunctionComponent<DispatchProps> = (props) => {
+  return (
+    <div className="not-trello">
+      
+      <Router>
+        <TopBar />
+        <div className="not-trello--content h-full">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/board/:id" component={Board} />
+            <Route path="/test/:id" component={Test} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
+        {props.modal === true ? (
+          <ModalWrapper>
+            <CardDetail />
+          </ModalWrapper>
+        ) : (
+          null
+        )}
+      </Router>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  modal: state.local.modal,
+});
+
+export default connect(mapStateToProps)(App);
